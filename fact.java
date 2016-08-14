ae0a88fd5b818a6fd1075a3ee02d513815f9731f
anuppama <anuppama.s24@gmail.com>
2016-08-14 12:03:13 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -4,12 +4,12 @@ class fact
 {
 public static void main(String args[])
 {
-Scanner s=new Scanner(System.in);
+		Scanner s=new Scanner(System.in);
 int num=s.nextInt();
 int fact=1;
 for(int i=num;i>=1;i--)
 {
-fact*= num;
+fact*= i;
 }
 if(num==0)
  System.out.println("1");
