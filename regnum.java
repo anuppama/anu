9ab22ac475b2d335176dfc98849cebc83250614e
anuppama <anuppama.s24@gmail.com>
2016-08-16 18:11:02 +0530

    Create regnum.java

---
+++ regnum.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class regnum
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int arr[]=new int[s.nextInt()];
+for(int i=0;i<arr.length;i++)
+{
+  n=s.nextInt();
+for(int j=0;j<arr.length;j++)
+{
+ if(n==arr[j])
+  System.out.println(n);
+else
+ arr[i]=n;
+}
+}
+}
+}
