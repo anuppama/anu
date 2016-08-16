d587710a5026debec76fb04ec121e1bce1d2f464
anuppama <anuppama.s24@gmail.com>
2016-08-16 18:34:30 +0530

    Update regnum.java

--- regnum.java
+++ regnum.java
@@ -7,20 +7,20 @@ public static void main(String args[])
 Scanner s=new Scanner(System.in);
 int arr[]=new int[s.nextInt()];
 int n=0;
-for(int i=0;i<arr.length;i++)
+int i,j;
+for(i=0;i<arr.length;i++)
 {
   n=s.nextInt();
-for(int j=0;j<arr.length;j++)
+for(j=0;j<arr.length;j++)
 {
  if(n==arr[j])
  {
-  System.out.println(n);
+  System.out.println("Repeated:"+n);
   break;
  }
 }
 if(j>=arr.length)
  arr[i]=n;
-}
 }
 }
 }
